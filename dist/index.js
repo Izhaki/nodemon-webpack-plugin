3c294aec4bae959ae076773d3ecd8b1d74407b3d
Izhaki <roey@gefri.org>
2018-04-10 23:16:59 +0100

    3.0.0

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,8 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+/* eslint-disable no-console */
+
 var nodemon = require('nodemon');
 var R = require('ramda');
 
@@ -11,63 +13,66 @@ var _require = require('./webpack-utils'),
     getOutputFileMeta = _require.getOutputFileMeta;
 
 module.exports = function () {
-    function _class(nodemonOptions) {
-        _classCallCheck(this, _class);
+  function _class(nodemonOptions) {
+    _classCallCheck(this, _class);
 
-        this.nodemonOptions = nodemonOptions;
-        this.isWebpackWatching = false;
-        this.isNodemonRunning = false;
+    this.nodemonOptions = nodemonOptions;
+    this.isWebpackWatching = false;
+    this.isNodemonRunning = false;
+  }
+
+  _createClass(_class, [{
+    key: 'apply',
+    value: function apply(compiler) {
+      var _this = this;
+
+      var OnAfterEmit = function OnAfterEmit(compilation, callback) {
+        if (_this.isWebpackWatching && !_this.isNodemonRunning) {
+          var _getOutputFileMeta = getOutputFileMeta(compilation),
+              relativeFileName = _getOutputFileMeta.relativeFileName;
+
+          _this.startMonitoring(relativeFileName);
+        }
+        callback();
+      };
+
+      var onWatchRun = function onWatchRun(comp, callback) {
+        _this.isWebpackWatching = true;
+        callback();
+      };
+
+      compiler.plugin('after-emit', OnAfterEmit);
+      compiler.plugin('watch-run', onWatchRun);
     }
+  }, {
+    key: 'startMonitoring',
+    value: function startMonitoring(relativeFileName) {
+      var nodemonOptionsDefaults = {
+        script: relativeFileName,
+        watch: relativeFileName
+      };
 
-    _createClass(_class, [{
-        key: 'apply',
-        value: function apply(compiler) {
-            var _this = this;
+      var nodemonOptions = R.merge(nodemonOptionsDefaults, this.nodemonOptions);
 
-            compiler.plugin('after-emit', function (compilation, callback) {
-                if (_this.isWebpackWatching && !_this.isNodemonRunning) {
-                    var _getOutputFileMeta = getOutputFileMeta(compilation),
-                        relativeFileName = _getOutputFileMeta.relativeFileName;
+      var monitor = nodemon(nodemonOptions);
 
-                    _this.startMonitoring(relativeFileName);
-                }
-                callback();
-            });
+      monitor.on('log', function (_ref) {
+        var colouredMessage = _ref.colour;
+        return console.log(colouredMessage);
+      });
 
-            compiler.plugin('watch-run', function (compiler, callback) {
-                _this.isWebpackWatching = true;
-                callback();
-            });
-        }
-    }, {
-        key: 'startMonitoring',
-        value: function startMonitoring(relativeFileName) {
-            var nodemonOptionsDefaults = {
-                script: relativeFileName,
-                watch: relativeFileName
-            };
+      this.isNodemonRunning = true;
 
-            var nodemonOptions = R.merge(nodemonOptionsDefaults, this.nodemonOptions);
+      // Ensure we exit nodemon when webpack exists.
+      process.once('exit', function () {
+        monitor.emit('exit');
+      });
+      // Ensure Ctrl-C triggers exit.
+      process.once('SIGINT', function () {
+        process.exit(0);
+      });
+    }
+  }]);
 
-            var monitor = nodemon(nodemonOptions);
-
-            monitor.on('log', function (_ref) {
-                var colouredMessage = _ref.colour;
-                return console.log(colouredMessage);
-            });
-
-            this.isNodemonRunning = true;
-
-            // Ensure we exit nodemon when webpack exists.
-            process.once('exit', function () {
-                monitor.emit('exit');
-            });
-            // Ensure Ctrl-C triggers exit.
-            process.once('SIGINT', function () {
-                process.exit(0);
-            });
-        }
-    }]);
-
-    return _class;
+  return _class;
 }();
