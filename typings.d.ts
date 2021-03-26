e6055e608620940d612c7b9fd30260e2eb062455
Izhaki <roey@gefri.org>
2021-03-26 15:20:54 +0000

    Fix: typings no published
    fixes #118

--- typings.d.ts
+++ typings.d.ts
@@ -1,10 +1,14 @@
-import { Compiler } from 'webpack';
 import { Settings as NodemonOptions } from 'nodemon';
 
+// We prioritise supporting CommonJS than ES6 syntax here.
+// Typescript users will have to add `esModuleInterop` in `compilerOptions`.
+// https://www.typescriptlang.org/docs/handbook/modules.html#export--and-import--require
 export = NodemonPlugin;
 
 declare class NodemonPlugin {
   constructor(options?: NodemonOptions);
 
-  apply(compiler: Compiler): void;
+  // yields error TS2321: Excessive stack depth comparing types 'NodemonPlugin' and '((this: Compiler, compiler: Compiler) => void) | WebpackPluginInstance'.
+  //apply(compiler: webpack.Compiler): void;
+  apply(compiler): void;
 }
